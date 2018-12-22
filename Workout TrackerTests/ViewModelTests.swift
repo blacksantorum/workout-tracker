49117f3d3b8c2b261de48138224e30cd3ffa9cdf
Christopher Tibbs <tibbs.17@gmail.com>
2018-12-22 15:22:59 -0800

    Add tests, mini storyboard

---
+++ Workout TrackerTests/ViewModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModelTests.swift
+//  Workout TrackerTests
+//
+//  Created by Chris Tibbs on 12/16/18.
+//  Copyright © 2018 Chris Tibbs. All rights reserved.
+//
+
+import XCTest
+
+class ViewModelTests: XCTestCase {
+
+    func testNoWorkoutsBeginningOfWeek() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
